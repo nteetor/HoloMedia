9cb976b15fa86ec0b3dd37054fdc3c2aaf6396da
nteetor <nate@haufin.ch>
2022-08-27 21:47:53 +0900

    initial commit

---
+++ HoloMedia.lua
@@ -0,0 +1,29 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND
+local MediaType_BORDER = LSM.MediaType.BORDER
+local MediaType_FONT = LSM.MediaType.FONT
+local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR
+
+-- -----
+-- BACKGROUND
+-- -----
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+LSM:Register(MediaType_FONT, "Calamity", [[Interface\Addons\SharedMedia_Holo\fonts\calamity\Calamity-Regular.ttf]])
+LSM:Register(MediaType_FONT, "Calamity Bold", [[Interface\Addons\SharedMedia_Holo\fonts\calamity\Calamity-Bold.ttf]])
+
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # HoloMedia
-Custom media for WeakAuras
+
+Custom media for my WeakAuras, https://wago.io/holodrum.
+
+## Installation
+
+1. Download this folder by clicking the green "Code" button and selecting "Download ZIP"
+1. Once you have the zip file download, extract the folder
+1. Move the folder to your AddOns folder,
+
+   On Windows, `C:\\World of Warcraft\_classic\Interface\Addons`
+   On Mac, `Macintosh HD/Applications/World of Warcraft/_classic_/Interface/Addons`
+
+   These are only the default folder locations, so double check you've got the right folder on your computer.
+1. Restart World of Warcraft if already running
